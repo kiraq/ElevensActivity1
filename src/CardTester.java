0f3b46a8b5bf72d9c5e7914f673d2bf893642615
BT_1N3_02 <kkhambhla0604@bths.edu>
2019-02-14 14:23:20 -0500

    done activity1

--- src/CardTester.java
+++ src/CardTester.java
@@ -2,4 +2,23 @@
  * Created by Teacher on 1/7/2019.
  */
 public class CardTester {
+
+    public static void main(String[] args){
+        Card card1 = new Card("King","Spades",10);
+        Card card2 = new Card("Six","Hearts",6);
+        Card card3 = new Card("Ace","Diamonds",1);
+        Card card4 = new Card("King","Spades",10);
+
+        System.out.println(card1);
+        System.out.println(card2);
+        System.out.println(card3);
+        System.out.println(card4);
+
+        System.out.println();
+        System.out.println(card1.matches(card4));
+        System.out.println(card2.suit());
+        System.out.println(card3.rank());
+        System.out.println(card2.pointValue());
+    }
+
 }
